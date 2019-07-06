c82e982ddd5122e0a7f306941feff0c591b30a1e
Marcin <marcin.aleksanderek@gmail.com>
2019-07-06 20:00:39 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://MongoUser2601:<password>@cluster0-qfnrp.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://MongoUser2601:MongoPass9768@cluster0-qfnrp.mongodb.net/test?retryWrites=true&w=majority', {
 	useMongoClient: true
 });
 
